7ec3dc560f47ab627393b7966c0cf24d55418753
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-07 17:09:03 +0000

    Posts list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { getPosts } from "@/lib/posts";
+
+export default async function Home() {
+  const fileNames = await getPosts();
+
   return (
-    <div className="">
-      Coucou
-    </div>
+    <ul className="list-disc list-inside">
+      {fileNames.map((fileName) => (
+        <li key={fileName}>{fileName}</li>
+      ))}
+    </ul>
   );
 }
