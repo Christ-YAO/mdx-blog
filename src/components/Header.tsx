4e48d864dca3c3532766773887dab2ec163e8f64
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-15 21:26:32 +0000

    change blog name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
 import Link from 'next/link'
-import React from 'react'
 import { ThemeToggle } from './theme/ThemeToggle'
+// import Headroom from 'react-headroom'
 
 export const Header = () => {
     return (
-        <header className="flex items-center justify-center border px-3 py-2 gap-2 bg-card shadow-xl rounded-3xl my-4 font-mono">
-            <p>
-                Codelynx<span className="text-primary">.dev</span>
-            </p>
+        // <Headroom>
+        <div className="flex items-center justify-center border px-3 py-2 gap-2 shadow-xl rounded-3xl my-4 font-mono top-4 z-10 backdrop:filter backdrop-blur-lg">
+            <Link href="/">
+                Christ<span className="text-primary">.dev</span>
+            </Link>
             <div className="ml-auto"></div>
             <Link href="/" className='text-primary hover:underline'>
                 Post
             </Link>
             <ThemeToggle />
-        </header>
+        </div>
+        // </Headroom>
     )
 }
