cbaff8c0bd15417856c3e1e5d479ec07f51ce4c1
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-16 17:02:58 +0000

    Generate metada and added view count on detail post page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,28 @@
 import Mdx from '@/lib/features/mdx/Mdx';
 import { getPost } from '@/lib/posts';
 import { notFound } from 'next/navigation';
+import { ViewCount } from './ViewCount';
+import { Metadata } from 'next';
+
+export const dynamic = "force-static"
+
+export const generateMetadata = async (props: {
+    params: { slug: string };
+}): Promise<Metadata> => {
+    const post = await getPost(props.params.slug);
+
+    if (!post) {
+        return {
+            title: "404 - Page Not Found",
+            description: "Page not found",
+        };
+    }
+
+    return {
+        title: post.title,
+        description: post.description,
+    };
+};
 
 const Page = async ({ params: { slug } }: { params: { slug: string } }) => {
     const post = await getPost(slug);
@@ -10,9 +32,12 @@ const Page = async ({ params: { slug } }: { params: { slug: string } }) => {
     }
     return (
         <div className='prose py-8'>
-            <p className="text-xs text-muted-foreground">
-                {new Date(post.publishedAt).toLocaleDateString()}
-            </p>
+            <div className="flex items-center gap-2">
+                <p className="text-xs text-muted-foreground">
+                    {new Date(post.publishedAt).toLocaleDateString()}
+                </p>
+                <ViewCount slug={slug} />
+            </div>
             <h1 className='dark:text-white'>{post.title}</h1>
             <Mdx>{post.content}</Mdx>
         </div>
