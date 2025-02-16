40f8b2a91a9005dbfb9879ae3c7b790a47254005
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-16 17:01:55 +0000

    Footer component final style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,15 @@
 import Link from 'next/link';
-import React from 'react';
 
 const Footer: React.FC = () => {
     return (
         <footer className="flex items-center justify-center border-t px-3 py-2 gap-2 font-mono">
-            <Link href="/">
-                &copy; {new Date().getFullYear()} | Christ<span className="text-primary">.dev</span>
-            </Link>
+            <p>
+                &copy; {new Date().getFullYear()} Copyright |
+            </p>
             <div className="ml-auto"></div>
-            {/* <Link href="/" className='text-primary hover:underline'>
-                Post
-            </Link> */}
+            <Link href="/" className='hover:underline'>
+                Christ<span className="text-primary">.dev</span>
+            </Link>
         </footer>
     );
 };
